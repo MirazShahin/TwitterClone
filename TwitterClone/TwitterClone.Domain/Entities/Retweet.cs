72569f5fb5931ae9e893c3c53fadd86ed526fd04
Miraz Hossain <mirazshahin847@gmail.com>
2026-08-08 22:05:37 +0600

    Updated Retweet class

--- TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace TwitterClone.Domain.Entities
 {
-    internal class ReTweet
+    public class Retweet
     {
+        private Guid _id;
+        private Guid _userId;
+        private User _user;
+
+        private Guid _orginalTweetId;
+        private Tweet _orginalTweet;
+        private DateTime _retweetAt;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+        public User User
+        {
+            get { return (User) _user; }
+            set { _user = value; }
+        }
+        public Guid OrginalTweetId
+        {
+            get { return _orginalTweetId; }
+        }
+        public Tweet OrginalTweet
+        {
+            get { return (Tweet) _orginalTweet;}
+            set { _orginalTweet = value; }
+        }
+        public DateTime RetweetAt
+        {
+            get { return RetweetAt; }
+            set { RetweetAt = value; }
+        }
     }
 }
