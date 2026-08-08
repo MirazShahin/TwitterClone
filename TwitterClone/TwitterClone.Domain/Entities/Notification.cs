76f4ddae0969d2a5bd72f2360d6255186f7958d2
Miraz Hossain <mirazshahin847@gmail.com>
2026-08-08 22:37:17 +0600

    Added Notification class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Notification.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TwitterClone.Domain.Entities
+{
+    public class Notification
+    {
+        private Guid _id;
+        private User _user;
+        private User _actor_user;
+        private Guid _actorId;
+        private DateTime _notificationTime;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+        public User ActorUser
+        {
+            get { return _actor_user; }
+            set { _actor_user = value; }
+        }
+        public Guid ActorId
+        {
+            get { return _actorId; }
+        }
+        public DateTime NotificationTime
+        {
+            get { return _notificationTime; }
+            set { _notificationTime = value; }
+        }
+    }
+}
