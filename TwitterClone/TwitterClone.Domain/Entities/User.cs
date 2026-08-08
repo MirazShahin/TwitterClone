23601d42e9ce9f20dfe9756f5844b85fad7bb1d0
Miraz Hossain <mirazshahin847@gmail.com>
2026-08-08 21:02:23 +0600

    Adding User, Tweet, Retweet, Like class

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id { get; set; }
+        private string _name { get; set; }
+        private string _email { get; set; }
+        private string _password { get; set; }
+        private string Gender { get; set; }
+        private string CreatedDate {  get; set; }
+            
+    }
+}
